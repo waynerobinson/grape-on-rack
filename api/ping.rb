fe7e478cc66065e5c792d7957b85c8eac49bd03e
Wayne Robinson <wayne.robinson@gmail.com>
2021-03-30 17:06:23 +1000

    Example: JSON Formatter Being Called Unbidden

--- api/ping.rb
+++ api/ping.rb
@@ -1,8 +1,33 @@
 module Acme
   class Ping < Grape::API
     format :json
+
     get '/ping' do
       { ping: 'pong' }
+    end
+
+    get '/ping_binary' do
+      env['api.format'] = :binary
+      content_type 'application/json'
+      { ping: 'pong' }.to_json
+    end
+
+    get '/ping_txt' do
+      env['api.format'] = :txt
+      content_type 'application/json'
+      { ping: 'pong' }.to_json
+    end
+
+    get '/ping_header' do
+      env['api.format'] = :binary
+      header 'Content-Type', 'application/json'
+      { ping: 'pong' }.to_json
+    end
+
+    get '/ping_other_header' do
+      env['api.format'] = :binary
+      content_type 'text/plain'
+      { ping: 'pong' }.to_json
     end
   end
 end
